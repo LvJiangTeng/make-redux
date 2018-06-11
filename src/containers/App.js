c5936c05f0fb1be0879f2387c71150562ae3a6ff
吕江腾 <lvjiangteng@126.com>
2018-06-11 09:27:29 +0800

    make-redux

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+class App extends React.Component {  
+  render() {  
+      return (  
+          <div>{this.props.children}</div>  
+      )  
+  }  
+}  
+export default App  
